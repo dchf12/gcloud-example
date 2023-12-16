625cc39860da4aa36a1a68e44f41c36a8ca488c1
dchf <dchf.jp@gmail.com>
2023-12-16 13:07:07 +0900

    middleware

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	log.Print("starting server...")
 	http.HandleFunc("/", handler)
+	http.Handle("/healthz", wrapHandlerWithLogging(MiddlewareLogging(http.HandlerFunc(Healthz))))
 
 	// Determine port for HTTP service.
 	port := os.Getenv("PORT")
@@ -23,6 +24,7 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -31,4 +33,40 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		name = "World"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", name)
+}
+
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func MiddlewareLogging(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		log.Printf("start %s\n", r.URL)
+		next.ServeHTTP(w, r)
+		log.Printf("finish %s\n", r.URL)
+	})
+}
+
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func NewLoggingResponseWriter(w http.ResponseWriter) *loggingResponseWriter {
+	return &loggingResponseWriter{w, http.StatusOK}
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func wrapHandlerWithLogging(wrapperHandler http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		lrw := NewLoggingResponseWriter(w)
+		log.Printf("wrapper handler\n")
+		wrapperHandler.ServeHTTP(lrw, r)
+		log.Printf("status code %d\n", lrw.statusCode)
+	})
 }
